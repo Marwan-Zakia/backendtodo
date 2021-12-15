d829ea728dc8ba64bcfcf55f7fa306a6bbf2920b
Marwan-Zakia <marwanamir.ma@gmail.com>
2021-12-15 19:27:38 +0200

    new

--- index.js
+++ index.js
@@ -17,7 +17,11 @@ app.use(cors({ origin: '*' }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+    });
 
 const DATABASE_URI =
   process.env.NODE_ENV === "test" ? "sqlite:memory:" : process.env.DATABASE_URL;
